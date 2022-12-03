7bdd82a7c2644187e4eb0563737f2139edc829d3
Chris Roche <github@rodaine.com>
2022-12-02 22:19:54 -0800

    dec03

---
+++ src/dec03/mod.rs
@@ -0,0 +1,78 @@
+use crate::utils::*;
+use std::collections::HashSet;
+use std::hash::Hash;
+
+fn char_set(s: &str) -> HashSet<char> {
+    s.chars().collect()
+}
+
+fn priority(ch: char) -> u32 {
+    if ch.is_ascii_lowercase() {
+        ch as u32 - 96
+    } else {
+        ch as u32 - 65 + 27
+    }
+}
+
+fn two_way_intersection<T>(a: HashSet<T>, b: HashSet<T>) -> impl Iterator<Item = T>
+where
+    T: Eq + Hash,
+{
+    a.into_iter().filter(move |el| b.contains(el))
+}
+
+fn three_way_intersection<T>(a: HashSet<T>, b: HashSet<T>, c: HashSet<T>) -> impl Iterator<Item = T>
+where
+    T: Eq + Hash,
+{
+    two_way_intersection(a, b).filter(move |el| c.contains(el))
+}
+
+fn solve1(input: &str) -> u32 {
+    file_lines(input)
+        .map(|l| {
+            let (a, b) = l.split_at(l.len() / 2);
+            (char_set(a), char_set(b))
+        })
+        .flat_map(|(a, b)| two_way_intersection(a, b))
+        .map(priority)
+        .sum()
+}
+
+fn solve2(input: &str) -> u32 {
+    file_lines(input)
+        .map(|l| char_set(&l))
+        .array_chunks()
+        .flat_map(|[a, b, c]| three_way_intersection(a, b, c))
+        .map(priority)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let input = "src/dec03/example_1.txt";
+        assert_eq!(157, solve1(input))
+    }
+
+    #[test]
+    fn puzzle_1() {
+        let input = "src/dec03/input_1.txt";
+        assert_eq!(7581, solve1(input))
+    }
+
+    #[test]
+    fn example_2() {
+        let input = "src/dec03/example_1.txt";
+        assert_eq!(70, solve2(input))
+    }
+
+    #[test]
+    fn puzzle_2() {
+        let input = "src/dec03/input_1.txt";
+        assert_eq!(2525, solve2(input))
+    }
+}
